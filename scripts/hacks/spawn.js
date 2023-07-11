9dcd86aea50e0b130a6dc72a721af6c1072cbf8f
mwinterstorm <mark.wr@gmail.com>
2023-07-12 09:59:40 +1200

    Update spawn.js
    To limit max threads

--- scripts/hacks/spawn.js
+++ scripts/hacks/spawn.js
@@ -1,24 +1,46 @@
 /** @param {NS} ns */
 export async function main(ns) {
-	ns.disableLog("ALL")
-	// ns.tail()
-	const server = ns.getHostname();
-	let maxram = ns.getServerMaxRam(server);
-	let usedram = ns.getServerUsedRam(server);
-	let freeram = maxram - usedram;
-	if (server == "home") {
-		freeram = freeram - 32
-	} 
-	let threads = Math.floor((freeram) / ns.getScriptRam("hacks/hack.js"))
-	if (threads >= 1) {
-		let report = "Starting hack.js with " + threads + " threads on " + server
-		await ns.print(report)
-		await ns.tryWritePort(8, report)
-		await ns.spawn("hacks/hack.js", threads, threads, server)
-	} else {
-		let report = "Not enough RAM"
-		await ns.print(report)
-		await ns.tryWritePort(8, report)
+	ns.disableLog("ALL");
+	// ns.tail();
 
-	}
+	// OPTIONS
+	let hack = "hacks/hack.js";
+	let spawn = "hacks/spawn.js"
+	let reserveRAM = 32; //RAM to be reserved on home computer
+	let maxThreads = 250; //Max threads to run on each instance
+
+	const server = ns.getHostname();
+	const maxram = ns.getServerMaxRam(server);
+	let usedram = ns.getServerUsedRam(server);
+	const hackRAM = ns.getScriptRam(hack);
+	const spawnRAM = ns.getScriptRam(spawn);
+	let freeram = maxram - usedram + spawnRAM;
+	if (server == "home") {
+		freeram = freeram - reserveRAM;
+	};
+	while (freeram >= hackRAM) {
+		let threads = Math.floor((freeram) / ns.getScriptRam(hack));
+		if (threads >= 1) {
+			if (threads > maxThreads ) {
+				threads = maxThreads;
+				let report = "Starting hack.js with " + threads + " threads on " + server
+				await ns.print(report)
+				await ns.tryWritePort(8, report)
+				await ns.run(hack, threads, threads, server);
+				freeram = freeram - (threads * hackRAM)
+			} else {
+				let report = "Starting hack.js with " + threads + " threads on " + server
+				await ns.print(report)
+				await ns.tryWritePort(8, report)
+				await ns.spawn(hack, threads, threads, server)
+				freeram = freeram - (threads * hackRAM)
+			};
+		} else {
+			let report = "Not enough RAM"
+			await ns.print(report)
+			await ns.tryWritePort(8, report)
+			
+		}
+		await ns.sleep(500)
+	};
 }
