0b7e77ea225f41c9c09b81418c2a29f786b8122b
mwinterstorm <mark.wr@gmail.com>
2023-07-12 14:50:31 +1200

    Install.js to be managed by coordinate.js

--- scripts/hacks/coordinate.js
+++ scripts/hacks/coordinate.js
@@ -1,52 +1,86 @@
 /** @param {NS} ns */
 export async function main(ns) {
-    ns.disableLog("ALL")
-    // ns.tail()
-    while (true) {
-        var servers = ns.scan()
-        for (let i = 0; i < servers.length; i++) {
-            if (ns.scriptRunning("hacks/self.js", servers[i])) {
-                if (ns.getServerMaxMoney(servers[i]) != 0) {
-                    const reqlev = ns.getServerRequiredHackingLevel(servers[i])
-                    const hacklev = ns.getHackingLevel()
-                    const highlimit = 0.66 * hacklev
-                    var lowlimit = 0.1 * hacklev
-                    if (hacklev > 6000) {
-                        lowlimit = 0.04 * hacklev
-                    }
-                    if (highlimit > reqlev) {
-                        if (Math.random() < (reqlev / lowlimit)) {
-                            while (!ns.tryWritePort(7, servers[i])) {
-                                await ns.sleep(100)
-                            }
-                            ns.print("SUCCESS hack.js add: " + servers[i])
-                        } else {
-                            if (Math.random() < 0.1) {
-                                while (!ns.tryWritePort(7, servers[i])) {
-                                    await ns.sleep(100)
-                                }
-                            } 
-                        }
-                    } else {
-                        ns.print("WARN Hack too hard: " + servers[i])
-                    }
-                } else {
-                    ns.print("FAIL No Money: " + servers[i])
-                }
-                await ns.sleep(100)
-            }
-            // then scan other servers and add
-            let nextservers = ns.scan(servers[i])
-            for (let n = 0; n < nextservers.length; n++) {
-                if (!servers.includes(nextservers[n])) {
-                    if (nextservers[n] != "home") {
-                        if (nextservers[n].includes("markwr") == false) {
-                            servers.push(nextservers[n])
-                            await ns.sleep(100)
-                        }
-                    }
-                }
-            }
-        }
-    }
+	ns.disableLog("ALL")
+	// ns.tail()
+	let installCount = 0
+	while (true) {
+		var servers = ns.scan()
+        let hackedServers = 0
+		for (let i = 0; i < servers.length; i++) {
+			if (ns.scriptRunning("hacks/self.js", servers[i])) {
+                hackedServers++
+				if (ns.getServerMaxMoney(servers[i]) != 0) {
+					const reqlev = ns.getServerRequiredHackingLevel(servers[i])
+					const hacklev = ns.getHackingLevel()
+					const highlimit = 0.66 * hacklev
+					var lowlimit = 0.1 * hacklev
+					if (hacklev > 6000) {
+						lowlimit = 0.04 * hacklev
+					}
+					if (highlimit > reqlev) {
+						if (Math.random() < (reqlev / lowlimit)) {
+							while (!ns.tryWritePort(7, servers[i])) {
+								await ns.sleep(100)
+							}
+							ns.print("SUCCESS hack.js add: " + servers[i])
+						} else {
+							if (Math.random() < 0.1) {
+								while (!ns.tryWritePort(7, servers[i])) {
+									await ns.sleep(100)
+								}
+								ns.print("SUCCESS hack.js add: " + servers[i])
+							} else {
+								ns.print("WARN skipped: " + servers[i])
+
+							}
+						}
+					} else {
+						ns.print("WARN Hack too hard: " + servers[i])
+					}
+				} else {
+					ns.print("FAIL No Money: " + servers[i])
+				}
+				await ns.sleep(100)
+			}
+			// then scan other servers and add
+			let nextservers = ns.scan(servers[i])
+			for (let n = 0; n < nextservers.length; n++) {
+				if (!servers.includes(nextservers[n])) {
+					if (nextservers[n] != "home") {
+						if (nextservers[n].includes("markwr") == false) {
+							servers.push(nextservers[n])
+							await ns.sleep(75)
+						}
+					}
+				}
+			}
+		}
+		if (installCount >= (10 * servers.length)) {
+			await ns.run("hacks/install.js");
+			let time = getTime();
+			let report = time + " - Running install.js..."
+			ns.print(report)
+			await ns.tryWritePort(8, report)
+			installCount = 0
+		} else {
+			let time = getTime();
+			installCount = installCount + (servers.length - hackedServers)
+			ns.print(time + ": " + ns.formatNumber((installCount / (10 * servers.length)*100), 4, 1000) + "% - progress to next INSTALL run")
+		}
+	}
+}
+
+function getTime() {
+	const d = new Date();
+	let hrs = d.getHours();
+	let hours = hrs;
+	if (hrs <= 9) { hours = "0" + hrs; }
+	let min = d.getMinutes();
+	let minutes = min;
+	if (min <= 9) { minutes = "0" + min; }
+	let sec = d.getSeconds();
+	let seconds = sec;
+	if (sec <= 9) { seconds = "0" + sec; }
+	let formattedTime = hours + ':' + minutes + ':' + seconds;
+	return formattedTime
 }
--- scripts/hacks/install.js
+++ scripts/hacks/install.js
@@ -5,87 +5,88 @@ export async function main(ns) {
 	// await ns.sleep(100);
 	// ns.moveTail(85, 495)
 	// ns.resizeTail(600, 300)
-	while (true) {
-		let time = getTime();
-		ns.print(time + " - Running install...")
-		const files = ["hacks/self.js"]
-		const servers = ns.scan()
-		const hacklev = ns.getHackingLevel()
-		var checkHacks = ["BruteSSH.exe", "FTPCrack.exe", "relaySMTP.exe", "HTTPWorm.exe", "SQLInject.exe"]
-		var hacks = []
-		for (let h = 0; h < checkHacks.length; h++) {
-			if (ns.fileExists(checkHacks[h], "home")) {
-				hacks.push(checkHacks[h])
-			}
+	const files = ["hacks/self.js"] //files to be copied, ensure index[0] is the one to run
+	const checkHacks = ["BruteSSH.exe", "FTPCrack.exe", "relaySMTP.exe", "HTTPWorm.exe", "SQLInject.exe"]
+	let time = getTime();
+	let report = time + " - Running install..."
+	ns.print(report);
+	// await ns.tryWritePort(8, report)
+	const servers = ns.scan()
+	const hacklev = ns.getHackingLevel()
+	var hacks = []
+	for (let h = 0; h < checkHacks.length; h++) {
+		if (ns.fileExists(checkHacks[h], "home")) {
+			hacks.push(checkHacks[h])
 		}
-		await ns.sleep(500)
-		for (let i = 0; i < servers.length; i++) {
-			if (servers[i].includes("markwr") == false) {
-				ns.scp(files, servers[i])
-				let hackreq = ns.getServerRequiredHackingLevel(servers[i])
-				if (ns.hasRootAccess(servers[i]) == true) {
-					if (!ns.isRunning("hacks/self.js", servers[i], servers[i])) {
-						if (hackreq <= hacklev) {
-							let threads = Math.floor(ns.getServerMaxRam(servers[i]) / ns.getScriptRam("hacks/self.js"))
-							if (threads >= 1) {
-								await ns.exec("hacks/self.js", servers[i], threads, servers[i])
-								let time = getTime();
-								ns.print(time + " - SUCCESS Running self.js with " + threads + " threads on " + servers[i])
-								await ns.sleep(500)
-							}
-						}
-					}
-				} else {
-					let portreq = ns.getServerNumPortsRequired(servers[i])
-					if (portreq <= hacks.length) {
-						for (let t = 0; t < hacks.length; t++) {
-							if (hacks[t] == "BruteSSH.exe") {
-								ns.brutessh(servers[i])
-							}
-							if (hacks[t] == "FTPCrack.exe") {
-								ns.ftpcrack(servers[i])
-							}
-							if (hacks[t] == "relaySMTP.exe") {
-								ns.relaysmtp(servers[i])
-							}
-							if (hacks[t] == "HTTPWorm.exe") {
-								ns.httpworm(servers[i])
-							}
-							if (hacks[t] == "SQLInject.exe") {
-								ns.sqlinject(servers[i])
-							}
-						}
-						await ns.nuke(servers[i])
-						let time = getTime();
-						ns.print(time + " - WARN Nuked " + servers[i])
-						await ns.sleep(500)
-						let threads = Math.floor(ns.getServerMaxRam(servers[i]) / 2.45)
+	}
+	await ns.sleep(500)
+	for (let i = 0; i < servers.length; i++) {
+		if (servers[i].includes("markwr") == false) {
+			ns.scp(files, servers[i])
+			let hackreq = ns.getServerRequiredHackingLevel(servers[i])
+			if (ns.hasRootAccess(servers[i]) == true) {
+				if (!ns.isRunning(files[0], servers[i], servers[i])) {
+					if (hackreq <= hacklev) {
+						let threads = Math.floor(ns.getServerMaxRam(servers[i]) / ns.getScriptRam(files[0]))
 						if (threads >= 1) {
-							await ns.exec("hacks/self.js", servers[i], threads, servers[i])
+							await ns.exec(files[0], servers[i], threads, servers[i])
 							let time = getTime();
-							ns.print(time + " - SUCCESS Running self.js with " + threads + " threads on " + servers[i])
+							let report = time + " - SUCCESS Running self.js with " + threads + " threads on " + servers[i]
+							ns.print(report)
+							await ns.tryWritePort(8, report)
 							await ns.sleep(500)
 						}
 					}
 				}
-			}
-			let nextservers = ns.scan(servers[i])
-			for (let n = 0; n < nextservers.length; n++) {
-				if (!servers.includes(nextservers[n])) {
-					if (nextservers[n] != "home") {
-						if (nextservers[n].includes("markwr") == false) {
-							servers.push(nextservers[n])
+			} else {
+				let portreq = ns.getServerNumPortsRequired(servers[i])
+				if (portreq <= hacks.length) {
+					for (let t = 0; t < hacks.length; t++) {
+						if (hacks[t] == "BruteSSH.exe") {
+							ns.brutessh(servers[i])
 						}
+						if (hacks[t] == "FTPCrack.exe") {
+							ns.ftpcrack(servers[i])
+						}
+						if (hacks[t] == "relaySMTP.exe") {
+							ns.relaysmtp(servers[i])
+						}
+						if (hacks[t] == "HTTPWorm.exe") {
+							ns.httpworm(servers[i])
+						}
+						if (hacks[t] == "SQLInject.exe") {
+							ns.sqlinject(servers[i])
+						}
+					}
+					await ns.nuke(servers[i])
+					let time = getTime();
+					ns.print(time + " - WARN Nuked " + servers[i])
+					await ns.sleep(500)
+					let threads = Math.floor(ns.getServerMaxRam(servers[i]) / 2.45)
+					if (threads >= 1) {
+						await ns.exec(files[0], servers[i], threads, servers[i])
+						let time = getTime();
+						let report = time + " - SUCCESS Running self.js with " + threads + " threads on " + servers[i]
+						ns.print(report)
+						await ns.tryWritePort(8, report)
+						await ns.sleep(500)
 					}
 				}
 			}
 		}
-		time = getTime();
-        let waitPause = Math.random() * 120000
-        ns.print("... complete, waiting: " + Math.floor(waitPause / 1000) + " seconds")
-        await ns.sleep(waitPause)
+		let nextservers = ns.scan(servers[i])
+		for (let n = 0; n < nextservers.length; n++) {
+			if (!servers.includes(nextservers[n])) {
+				if (nextservers[n] != "home") {
+					if (nextservers[n].includes("markwr") == false) {
+						servers.push(nextservers[n])
+					}
+				}
+			}
+		}
 	}
 }
+
 
 function getTime() {
 	const d = new Date();
